38209fc33583a73b602e12e2a7b82569a593759c
Nebeyou Musie <120266392+NebeyouMusie@users.noreply.github.com>
2024-06-24 09:34:10 +0000

    update file

---
+++ langraph_intro.py
@@ -0,0 +1,59 @@
+from typing import Annotated, Literal, TypedDict
+
+from langchain_core.messages import HumanMessage
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_groq import ChatGroq
+from langgraph.checkpoint import MemorySaver
+from langgraph.graph import END, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Define the tools for the agent to use
+tools = [TavilySearchResults(max_results=1, api_wrapper=os.getenv('TAVILY_API_KEY'))]
+tool_node = ToolNode(tools)
+
+model = ChatGroq(temperature=0.5, api_key=os.getenv('GROQ_API_KEY'), model="mixtral-8x7b-32768")
+
+# print(model.invoke('hello'))
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state):
+    messages = state['messages']
+    last_message = messages[-1]
+    # If the LLM makes a tool call, then we route to teh 'tools' node
+    if last_message.tool_calls:
+        return "tools"
+    return END
+
+
+# Define the function that calls the model
+def call_model(state):
+    messages = state['messages']
+    response = model.invoke(messages)
+    # We return a list, because this will get added to teh existing list
+    return {"messages": [response]}
+
+# Define a new Graph
+workflow = StateGraph(MessagesState)
+
+#Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("tools", tool_node)
+
+# Set the entrypoint as 'agent'
+# This means that this node is the first one called
+workflow.set_entry_point("agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use 'agent'.
+    # This means there are the edges taken after the 'agent' node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+)
